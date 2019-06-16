509c1225cf5eda6c9a45be64aa27062eb3ffe245
HKman2018 <45880252+HKman2018@users.noreply.github.com>
2019-06-16 23:26:28 +0800

    Update handlebarsHelpers.js

--- handlebarsHelpers.js
+++ handlebarsHelpers.js
@@ -1,5 +1,5 @@
-const hbs = require('handlebars')
-hbs.registerHelper('ifEquals', (value1, value2) => {
+const Handlebars = require('handlebars')
+
+Handlebars.registerHelper('ifEquals', (value1, value2) => {
   return (value1 === value2) ? 'checked' : ''
-  console.log(value1)
 })
